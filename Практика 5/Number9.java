e9e27b3f927b1515931592c256d061e9bc88d1d1
microsoftcraft <70655677+microsoftcraft@users.noreply.github.com>
2020-09-13 20:50:39 +0300

    q
    q

---
+++ Практика 5/Number9.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Number9
+{
+    public static int recursion(int a, int b) {
+    if (a > b + 1) {
+        return 0;
+    }
+    if (a == 0 || b == 0) {
+        return 1;
+    }
+    return recursion(a, b - 1) + recursion(a - 1, b - 1);
+}
+    public static void main(String[] args) {
+        System.out.println(recursion(5, 8));
+    }
+}
+
+//Даны числа a и b. Определите, сколько существует последовательностей
+//из a нулей и b единиц, в которых никакие два нуля не стоят рядом.
