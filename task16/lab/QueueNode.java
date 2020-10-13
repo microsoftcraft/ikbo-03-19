00ea075d02a8df991e2ca642803621e5518a4102
microsoftcraft <70655677+microsoftcraft@users.noreply.github.com>
2020-10-13 16:18:12 +0300

    Update QueueNode.java

--- task16/lab/QueueNode.java
+++ task16/lab/QueueNode.java
@@ -3,4 +3,11 @@ package task16.lab;
 public class QueueNode {
     private  QueueNode next,prev;
     private Order value;
+    
+    QueueNode(QueueNode prev, Order value, QueueNode next)
+    {
+        this.value = value;
+        this.next = next;
+        this.prev = prev;
+    }
 }
