5d2fd36be3a1e0620ab9308bae509d788e363a29
microsoftcraft <70655677+microsoftcraft@users.noreply.github.com>
2020-10-13 16:14:46 +0300

    Update Customer.java

--- task16/lab/Customer.java
+++ task16/lab/Customer.java
@@ -6,6 +6,12 @@ public class Customer {
     private Address address;
     private Customer MATURE_UNKNOWN_CUSTOMER, NOT_MATURE_UNKNOWN_CUSTOMER;
 
+    public Customer(String firstName,String secondName, int age)
+    {
+        this.firstName=firstName;
+        this.secondName=secondName;
+        this.age=age;
+    }
     public String getFirstName()
     {
         return  firstName;
