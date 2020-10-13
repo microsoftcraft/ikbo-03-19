e3c375d5890383999764e5888b9fb7feab8dbb68
microsoftcraft <70655677+microsoftcraft@users.noreply.github.com>
2020-10-13 16:16:32 +0300

    Update MenuItem.java

--- task16/lab/MenuItem.java
+++ task16/lab/MenuItem.java
@@ -5,6 +5,13 @@ public class MenuItem
     private int cost;
     private String name, description;
 
+    public MenuItem( String name,String description, int cost)
+    {
+        this.name=name;
+        thisdescription=description;
+        this.cost=cost;
+    }
+    
     public int getCost()
     {
         return cost;
